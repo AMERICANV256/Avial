64845db09437e9cec4d9bea76907d31f439530ee
AMERICANV256 <matiaspineda@americanvial.com>
2024-10-23 12:58:53 -0300

    nada

--- api/src/controllers/Cotizaciones.js
+++ api/src/controllers/Cotizaciones.js
@@ -1100,7 +1100,10 @@ const getUltimasCotizaciones = async (req, res) => {
       return res.status(401).send({ error: "Token no proporcionado" });
     }
 
-    const decodedToken = jwt.decode(token.replace("Bearer ", ""), JWTSECRET);
+    const decodedToken = jwt.decodeToken(
+      token.replace("Bearer ", ""),
+      JWTSECRET
+    );
 
     const idUsuario = decodedToken.id;
 
@@ -1115,40 +1118,21 @@ const getUltimasCotizaciones = async (req, res) => {
       return res.status(404).json({ error: "Usuario no encontrado" });
     }
 
-    // Definir las condiciones de búsqueda para cotizaciones
+    // Definir las condiciones de búsqueda
     let whereCondition = {};
-
     if (usuario.rol === false && !usuario.baneado) {
-      // Si el usuario no está baneado y tiene rol "false", filtrar solo sus cotizaciones
       whereCondition.idUsuario = idUsuario;
-    } else if (usuario.rol === true && !usuario.baneado) {
-      // Si el usuario tiene rol "true" (distribuidor)
-      if (!usuario.distribuidor) {
-        // Si el distribuidor es nulo, no filtramos por distribuidor, obtenemos todas las cotizaciones
-      } else {
-        // Si tiene distribuidor, filtrar cotizaciones propias y de otros con el mismo distribuidor
-        whereCondition = {
-          [Op.or]: [
-            { idUsuario: usuario.id }, // Cotizaciones del propio usuario
-            {
-              "$Usuarios.distribuidor$": usuario.distribuidor, // Cotizaciones de otros usuarios con el mismo distribuidor
-              "$Usuarios.rol$": false, // Solo de usuarios con rol "false"
-            },
-          ],
-        };
-      }
-    } else if (usuario.baneado) {
-      // Si el usuario está baneado, no tiene acceso
-      return res
-        .status(403)
-        .json({ error: "Usuario baneado, no tiene permisos para acceder" });
+    } else if (
+      usuario.rol === true ||
+      (usuario.rol === false && usuario.baneado === true)
+    ) {
     } else {
       return res
         .status(403)
         .send({ error: "No tienes permisos para acceder a esta información" });
     }
 
-    // Obtener las últimas 5 cotizaciones con las condiciones definidas
+    // Obtener las últimas 5 cotizaciones
     const cotizaciones = await Cotizaciones.findAll({
       where: whereCondition,
       order: [["fechaDeCreacion", "DESC"]],
@@ -1165,10 +1149,6 @@ const getUltimasCotizaciones = async (req, res) => {
         {
           model: CotizacionIndividual,
           attributes: ["PrecioFinal"],
-        },
-        {
-          model: Usuarios,
-          attributes: [],
         },
       ],
     });
