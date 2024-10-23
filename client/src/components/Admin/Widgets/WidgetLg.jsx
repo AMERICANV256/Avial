d451dded6e52eaefcf111604b6499c5f98569933
Matu36 <matipineda85@live.com.ar>
2024-10-23 19:31:03 -0300

    varios

--- client/src/components/Admin/Widgets/WidgetLg.jsx
+++ client/src/components/Admin/Widgets/WidgetLg.jsx
@@ -11,6 +11,8 @@ export default function WidgetLg() {
   const { data, isLoading } =
     useUltimasCotizaciones(idUsuario).ultimasCotizacionesQuery;
 
+  console.log(data);
+
   if (isLoading) {
     return (
       <div>
@@ -33,25 +35,28 @@ export default function WidgetLg() {
           </tr>
         </thead>
         <tbody>
-          {data
-            ?.flatMap((cotizacion) =>
-              cotizacion.CotizacionIndividuals.map((individual, index) => ({
-                cotizacion,
-                individual,
-                index,
-              }))
-            )
-            .slice(0, 5)
-            .map(({ cotizacion, individual, index }) => (
-              <tr key={`${cotizacion.id}-${index}`}>
-                <td>
-                  {cotizacion.Cliente.nombre} {cotizacion.Cliente.apellido}
-                </td>
-                <td>{cotizacion.Producto.modelo}</td>
-                <td>U$D</td>
-                <td>{individual.PrecioFinal}</td>
-              </tr>
-            ))}
+          {Array.isArray(data?.cotizaciones) &&
+            data.cotizaciones
+              .reduce((acc, cotizacion) => {
+                const cotizacionesConIndividuals =
+                  cotizacion.CotizacionIndividuals.map((individual, index) => ({
+                    cotizacion,
+                    individual,
+                    index,
+                  }));
+                return acc.concat(cotizacionesConIndividuals);
+              }, [])
+              .slice(0, 5)
+              .map(({ cotizacion, individual, index }) => (
+                <tr key={`${cotizacion.id}-${index}`}>
+                  <td>
+                    {cotizacion.Cliente.nombre} {cotizacion.Cliente.apellido}
+                  </td>
+                  <td>{cotizacion.Producto.modelo}</td>
+                  <td>U$D</td>
+                  <td>{individual.PrecioFinal}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
